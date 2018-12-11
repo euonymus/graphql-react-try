18c6af8012b6501d22204585eac684982d22d0b8
euonymus <euonymus0220@gmail.com>
2018-12-11 16:51:38 +0900

    vote

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,15 +3,49 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
+// const FEED_QUERY = gql`
+//   {
+//     links(search:"Jonatas", first:3) {
+//       id
+//       url
+//       description
+//     }
+//   }
+// `
+// const FEED_QUERY = gql`
+//   {
+//     links {
+//       id
+//       url
+//       description
+//     }
+//   }
+// `
 const FEED_QUERY = gql`
   {
-    links(search:"Jonatas", first:3) {
+    links {
       id
       url
       description
+      postedBy {
+        id
+        username
+      }
+      votes {
+        edges {
+          node {
+            id
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
     }
   }
 `
+
 
 class LinkList extends Component {
   render() {
@@ -24,9 +58,11 @@ class LinkList extends Component {
            const linksToRender = data.links
 
            return (
-           <div>
-             {linksToRender.map(link => <Link key={link.id} link={link} />)}
-           </div>
+             <div>
+               {linksToRender.map((link, index) => (
+                 <Link key={link.id} link={link} index={index} />
+               ))}
+             </div>
            )
         }}
       </Query>
