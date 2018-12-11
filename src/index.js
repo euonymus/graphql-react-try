5cbee91d703f170df2d9048fe11e3580bd71d3f3
euonymus <euonymus0220@gmail.com>
2018-12-11 19:45:31 +0900

    try web socket

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
 import { AUTH_TOKEN } from './constants'
 
 const httpLink = createHttpLink({
